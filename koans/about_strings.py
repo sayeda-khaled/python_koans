5bd79415b4d79c931c88cd4b2147a65e02c54177
Sayeda Khaled <sayedakhaled@Sayedas-MacBook-Pro.local>
2021-06-07 15:29:34 -0400

    cleared the errors in the about_strings folder

--- koans/about_strings.py
+++ koans/about_strings.py
@@ -146,10 +146,10 @@ world!
 """
         self.assertEqual(15, len(string))
 
-    # def test_triple_quoted_strings_need_less_escaping(self):
-    #     a = "Hello \"world\"."
-    #     b = """Hello "world"."""
-    #     self.assertEqual(__, (a == b))
+    #def test_triple_quoted_strings_need_less_escaping(self):
+    #  a = "Hello \"world\"."
+    #  b = """Hello "world"."""
+    #  self.assertEqual(__, (a == b))
 
     #We only need to use escape characters if we are using double quotes and have a quotation or single
     #quotation and need ot use an apostrophe. With the triple quotes, we won't need to use the escaping
@@ -159,40 +159,96 @@ world!
         b = """Hello "world"."""
         self.assertEqual(True, (a == b))
 
+    # def test_escaping_quotes_at_the_end_of_triple_quoted_string(self):
+    #     string = """Hello "world\""""
+    #     self.assertEqual(__, string)
+
+    #https://www.digitalocean.com/community/tutorials/how-to-format-text-in-python-3
+    #When we use triple quotes, we will see that there is a space at the top and bottom when we print the string.
+    #We can remove those spaces by using the \ escape key.. The escape character here is to eliminate the space
+    #at the bottom.
+
     def test_escaping_quotes_at_the_end_of_triple_quoted_string(self):
         string = """Hello "world\""""
-        self.assertEqual(__, string)
+        self.assertEqual('Hello "world"', string)
 
+    #
+    # def test_plus_concatenates_strings(self):
+    #     string = "Hello, " + "world"
+    #     self.assertEqual(__, string)
+
+    #The plus sign here is concatenating the two strings..
     def test_plus_concatenates_strings(self):
         string = "Hello, " + "world"
-        self.assertEqual(__, string)
+        self.assertEqual("Hello, world", string)
 
+
+    # def test_adjacent_strings_are_concatenated_automatically(self):
+    #     string = "Hello" ", " "world"
+    #     self.assertEqual(__, string)
+    #https://docs.python.org/3/reference/lexical_analysis.html
     def test_adjacent_strings_are_concatenated_automatically(self):
         string = "Hello" ", " "world"
-        self.assertEqual(__, string)
+        self.assertEqual("Hello, world", string)
+
+
+
+    # def test_plus_will_not_modify_original_strings(self):
+    #     hi = "Hello, "
+    #     there = "world"
+    #     string = hi + there
+    #     self.assertEqual(__, hi)
+    #     self.assertEqual(__, there)
+
+    # https://web.stanford.edu/class/archive/cs/cs106a/cs106a.1204/handouts/py-string.html
+    # The + operator combines (aka "concatenates") two strings to make a bigger string.
+    # This creates new strings to represent the result, leaving the original strings unchanged.
 
     def test_plus_will_not_modify_original_strings(self):
         hi = "Hello, "
         there = "world"
         string = hi + there
-        self.assertEqual(__, hi)
-        self.assertEqual(__, there)
+        self.assertEqual("Hello, ", hi)
+        self.assertEqual("world", there)
+
+
+    #def test_plus_equals_will_append_to_end_of_string(self):
+    #    hi = "Hello, "
+    #    there = "world"
+    #    hi += there
+    #    self.assertEqual(__, hi)
+    #the += will append the string at the end and that will mutate the original string
 
     def test_plus_equals_will_append_to_end_of_string(self):
         hi = "Hello, "
         there = "world"
         hi += there
-        self.assertEqual(__, hi)
+        self.assertEqual("Hello, world", hi)
+
+    # def test_plus_equals_also_leaves_original_string_unmodified(self):
+    #     original = "Hello, "
+    #     hi = original
+    #     there = "world"
+    #     hi += there
+    #     self.assertEqual(__, original)
+    #tested it in the console. The original string was not modified, only the string with the += operators
 
     def test_plus_equals_also_leaves_original_string_unmodified(self):
         original = "Hello, "
         hi = original
         there = "world"
         hi += there
-        self.assertEqual(__, original)
+        self.assertEqual("Hello, ", original)
+    #
+    # def test_most_strings_interpret_escape_characters(self):
+    #     string = "\n"
+    #     self.assertEqual('\n', string)
+    #     self.assertEqual("""\n""", string)
+    #     self.assertEqual(__, len(string))
+    #Escape character is recognized by the string and the value is 1 for the string length.
 
     def test_most_strings_interpret_escape_characters(self):
         string = "\n"
         self.assertEqual('\n', string)
         self.assertEqual("""\n""", string)
-        self.assertEqual(__, len(string))
+        self.assertEqual(1, len(string))
