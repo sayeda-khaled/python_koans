a5cbe363173edc831de237a184d765c921fb7ce7
Sayeda Khaled <sayedakhaled@Sayedas-MacBook-Pro.local>
2021-06-13 18:42:34 -0400

    completed the about_methods

--- koans/about_methods.py
+++ koans/about_methods.py
@@ -7,15 +7,36 @@
 
 from runner.koan import *
 
+# def my_global_function(a,b):
+#     return a + b
+#
+# class AboutMethods(Koan):
+#     def test_calling_a_global_function(self):
+#         self.assertEqual(__, my_global_function(2,3))
+
+# https://www.programiz.com/python-programming/function
+# variables outside of the function are visible from inside. They have a global scope.
 def my_global_function(a,b):
     return a + b
 
 class AboutMethods(Koan):
     def test_calling_a_global_function(self):
-        self.assertEqual(__, my_global_function(2,3))
+        self.assertEqual(5, my_global_function(2,3))
+
+
+    # def test_calling_functions_with_wrong_number_of_arguments(self):
+    #     try:
+    #         my_global_function()
+    #     except TypeError as exception:
+    #         msg = exception.args[0]
 
     # NOTE: Wrong number of arguments is not a SYNTAX error, but a
     # runtime error.
+    # https://www.w3schools.com/python/python_functions.asp
+    # By default, a function must be called with the correct number of arguments.
+    # Meaning that if your function expects 2 arguments, you have to call the function
+    # with 2 arguments, not more, and not less.
+
     def test_calling_functions_with_wrong_number_of_arguments(self):
         try:
             my_global_function()
@@ -33,38 +54,90 @@ class AboutMethods(Koan):
             msg = e.args[0]
 
         # Note, watch out for parenthesis. They need slashes in front!
-        self.assertRegex(msg, __)
+        self.assertRegex(msg, r'my_global_function\(\) takes 2 positional arguments but 3 were given')
 
     # ------------------------------------------------------------------
+
+    # def pointless_method(self, a, b):
+    #     sum = a + b
+    #
+    # def test_which_does_not_return_anything(self):
+    #     self.assertEqual(__, self.pointless_method(1, 2))
+    #     # Notice that methods accessed from class scope do not require
+    #     # you to pass the first "self" argument?
+
+    # https://www.knowledgehut.com/blog/programming/self-variabe-python-examples
+    # self represents the instance or objects of a class and binds the attributes of a class with specific arguments.
+    # The use of self variable in Python helps to differentiate between the instance attributes (and methods) and
+    # local variables.
 
     def pointless_method(self, a, b):
         sum = a + b
 
     def test_which_does_not_return_anything(self):
-        self.assertEqual(__, self.pointless_method(1, 2))
+        self.assertEqual(None, self.pointless_method(1, 2))
         # Notice that methods accessed from class scope do not require
         # you to pass the first "self" argument?
 
     # ------------------------------------------------------------------
 
+    # def method_with_defaults(self, a, b='default_value'):
+    #     return [a, b]
+    #
+    # def test_calling_with_default_values(self):
+    #     self.assertEqual(__, self.method_with_defaults(1))
+    #     self.assertEqual(__, self.method_with_defaults(1, 2))
+
+    # https://www.geeksforgeeks.org/default-arguments-in-python/
+    # Python allows function arguments to have default values.
+    # If the function is called without the argument, the argument gets its default value.
+
     def method_with_defaults(self, a, b='default_value'):
         return [a, b]
 
     def test_calling_with_default_values(self):
-        self.assertEqual(__, self.method_with_defaults(1))
-        self.assertEqual(__, self.method_with_defaults(1, 2))
+        self.assertEqual([1, 'default_value'], self.method_with_defaults(1))
+        self.assertEqual([1, 2], self.method_with_defaults(1, 2))
 
     # ------------------------------------------------------------------
 
+    # def method_with_var_args(self, *args):
+    #     return args
+    #
+    # def test_calling_with_variable_arguments(self):
+    #     self.assertEqual(__, self.method_with_var_args())
+    #     self.assertEqual(('one',), self.method_with_var_args('one'))
+    #     self.assertEqual(__, self.method_with_var_args('one', 'two'))
+
+    # https://www.geeksforgeeks.org/default-arguments-in-python/
+    # https://www.tutorialspoint.com/python/python_functions.htm
     def method_with_var_args(self, *args):
         return args
 
     def test_calling_with_variable_arguments(self):
-        self.assertEqual(__, self.method_with_var_args())
+        self.assertEqual((), self.method_with_var_args())
         self.assertEqual(('one',), self.method_with_var_args('one'))
-        self.assertEqual(__, self.method_with_var_args('one', 'two'))
+        self.assertEqual(('one', 'two'), self.method_with_var_args('one', 'two'))
+
 
     # ------------------------------------------------------------------
+
+    # def function_with_the_same_name(self, a, b):
+    #     return a + b
+    #
+    # def test_functions_without_self_arg_are_global_functions(self):
+    #     def function_with_the_same_name(a, b):
+    #         return a * b
+    #
+    #     self.assertEqual(__, function_with_the_same_name(3,4))
+    #
+    # def test_calling_methods_in_same_class_with_explicit_receiver(self):
+    #     def function_with_the_same_name(a, b):
+    #         return a * b
+    #
+    #     self.assertEqual(__, self.function_with_the_same_name(3,4))
+
+    # https://www.geeksforgeeks.org/python-call-function-from-another-function/
 
     def function_with_the_same_name(self, a, b):
         return a + b
@@ -73,15 +146,29 @@ class AboutMethods(Koan):
         def function_with_the_same_name(a, b):
             return a * b
 
-        self.assertEqual(__, function_with_the_same_name(3,4))
+        self.assertEqual(12, function_with_the_same_name(3,4))
 
     def test_calling_methods_in_same_class_with_explicit_receiver(self):
         def function_with_the_same_name(a, b):
             return a * b
 
-        self.assertEqual(__, self.function_with_the_same_name(3,4))
+        self.assertEqual(7, self.function_with_the_same_name(3,4))
 
     # ------------------------------------------------------------------
+
+    # def another_method_with_the_same_name(self):
+    #     return 10
+    #
+    # link_to_overlapped_method = another_method_with_the_same_name
+    #
+    # def another_method_with_the_same_name(self):
+    #     return 42
+    #
+    # def test_that_old_methods_are_hidden_by_redefinitions(self):
+    #     self.assertEqual(__, self.another_method_with_the_same_name())
+    #
+    # def test_that_overlapped_method_is_still_there(self):
+    #     self.assertEqual(__, self.link_to_overlapped_method())
 
     def another_method_with_the_same_name(self):
         return 10
@@ -92,43 +179,120 @@ class AboutMethods(Koan):
         return 42
 
     def test_that_old_methods_are_hidden_by_redefinitions(self):
-        self.assertEqual(__, self.another_method_with_the_same_name())
+        self.assertEqual(42, self.another_method_with_the_same_name())
 
     def test_that_overlapped_method_is_still_there(self):
-        self.assertEqual(__, self.link_to_overlapped_method())
+        self.assertEqual(10, self.link_to_overlapped_method())
 
     # ------------------------------------------------------------------
+
+    # def empty_method(self):
+    #     pass
+    #
+    # def test_methods_that_do_nothing_need_to_use_pass_as_a_filler(self):
+    #     self.assertEqual(__, self.empty_method())
+    #
+    # def test_pass_does_nothing_at_all(self):
+    #     "You"
+    #     "shall"
+    #     "not"
+    #     pass
+    #     self.assertEqual(____, "Still got to this line" != None)
+    #
+    # https://www.programiz.com/python-programming/pass-statement
+    #
+    # In Python programming, the pass statement is a null statement.
+    # The difference between a comment and a pass statement in Python is tha
+    # while the interpreter ignores a comment entirely, pass is not ignored.
+    # However, nothing happens when the pass is executed. It results in no operation (NOP).
 
     def empty_method(self):
         pass
 
     def test_methods_that_do_nothing_need_to_use_pass_as_a_filler(self):
-        self.assertEqual(__, self.empty_method())
+        self.assertEqual(None, self.empty_method())
 
     def test_pass_does_nothing_at_all(self):
         "You"
         "shall"
         "not"
         pass
-        self.assertEqual(____, "Still got to this line" != None)
+        self.assertEqual(True, "Still got to this line" != None)
 
     # ------------------------------------------------------------------
+
+    # def one_line_method(self): return 'Madagascar'
+    #
+    # def test_no_indentation_required_for_one_line_statement_bodies(self):
+    #     self.assertEqual(__, self.one_line_method())
+
+    # https://www.programiz.com/python-programming/statement-indentation-comments
+    # single line method, do ot require indentation
 
     def one_line_method(self): return 'Madagascar'
 
     def test_no_indentation_required_for_one_line_statement_bodies(self):
-        self.assertEqual(__, self.one_line_method())
+        self.assertEqual('Madagascar', self.one_line_method())
 
     # ------------------------------------------------------------------
+
+    # def method_with_documentation(self):
+    #     "A string placed at the beginning of a function is used for documentation"
+    #     return "ok"
+    #
+    # def test_the_documentation_can_be_viewed_with_the_doc_method(self):
+    #     self.assertRegex(self.method_with_documentation.__doc__, __)
+
+    # https://www.geeksforgeeks.org/python-docstrings/
+    #
+    # Python documentation strings (or docstrings) provide a convenient way of associating documentation
+    # with Python modules, functions, classes, and methods. It’s specified in source code that is used, like a comment,
+    # to document a specific segment of code. Unlike conventional source code comments,
+    # the docstring should describe what the function does, not how.
 
     def method_with_documentation(self):
         "A string placed at the beginning of a function is used for documentation"
         return "ok"
 
     def test_the_documentation_can_be_viewed_with_the_doc_method(self):
-        self.assertRegex(self.method_with_documentation.__doc__, __)
+        self.assertRegex(self.method_with_documentation.__doc__, "A string placed at the beginning of a function is used for documentation")
+
 
     # ------------------------------------------------------------------
+    #
+    # class Dog:
+    #     def name(self):
+    #         return "Fido"
+    #
+    #     def _tail(self):
+    #         # Prefixing a method with an underscore implies private scope
+    #         return "wagging"
+    #
+    #     def __password(self):
+    #         return 'password' # Genius!
+    #
+    # def test_calling_methods_in_other_objects(self):
+    #     rover = self.Dog()
+    #     self.assertEqual(__, rover.name())
+    #
+    # def test_private_access_is_implied_but_not_enforced(self):
+    #     rover = self.Dog()
+    #
+    #     # This is a little rude, but legal
+    #     self.assertEqual(__, rover._tail())
+    #
+    # def test_attributes_with_double_underscore_prefixes_are_subject_to_name_mangling(self):
+    #     rover = self.Dog()
+    #     with self.assertRaises(___): password = rover.__password()
+    #
+    #     # But this still is!
+    #     self.assertEqual(__, rover._Dog__password())
+    #
+    #     # Name mangling exists to avoid name clash issues when subclassing.
+    #     # It is not for providing effective access protection
+
+    # https://www.geeksforgeeks.org/python-classes-and-objects/
+    # https://www.geeksforgeeks.org/name-mangling-in-python/
 
     class Dog:
         def name(self):
@@ -143,20 +307,20 @@ class AboutMethods(Koan):
 
     def test_calling_methods_in_other_objects(self):
         rover = self.Dog()
-        self.assertEqual(__, rover.name())
+        self.assertEqual("Fido", rover.name())
 
     def test_private_access_is_implied_but_not_enforced(self):
         rover = self.Dog()
 
         # This is a little rude, but legal
-        self.assertEqual(__, rover._tail())
+        self.assertEqual("wagging", rover._tail())
 
     def test_attributes_with_double_underscore_prefixes_are_subject_to_name_mangling(self):
         rover = self.Dog()
-        with self.assertRaises(___): password = rover.__password()
+        with self.assertRaises(AttributeError): password = rover.__password()
 
         # But this still is!
-        self.assertEqual(__, rover._Dog__password())
+        self.assertEqual('password', rover._Dog__password())
 
         # Name mangling exists to avoid name clash issues when subclassing.
         # It is not for providing effective access protection
